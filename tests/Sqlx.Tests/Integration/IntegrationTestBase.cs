6ad447ee3ee3354f29d029afa00ad0d3f64e2e73
HCricle <15218450198@163.com>
2025-12-22 19:30:02 +0800

    添加IntegrationTestBase基类，简化测试数据管理

---
+++ tests/Sqlx.Tests/Integration/IntegrationTestBase.cs
@@ -0,0 +1,45 @@
+// -----------------------------------------------------------------------
+// <copyright file="IntegrationTestBase.cs" company="Cricle">
+// Copyright (c) Cricle. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sqlx;
+
+namespace Sqlx.Tests.Integration;
+
+/// <summary>
+/// 集成测试基类
+/// 提供通用的测试初始化和清理逻辑
+/// </summary>
+public abstract class IntegrationTestBase
+{
+    protected static DatabaseFixture _fixture = null!;
+    protected bool _needsSeedData = false;  // 子类可以设置是否需要预置数据
+
+    [ClassInitialize]
+    public static void BaseClassInitialize(TestContext context)
+    {
+        _fixture = new DatabaseFixture();
+    }
+
+    [ClassCleanup]
+    public static void BaseClassCleanup()
+    {
+        _fixture?.Dispose();
+    }
+
+    [TestInitialize]
+    public virtual void TestInitialize()
+    {
+        // 每个测试前清理数据
+        _fixture.CleanupData(SqlDefineTypes.SQLite);
+        
+        // 如果需要，插入测试数据
+        if (_needsSeedData)
+        {
+            _fixture.SeedTestData(SqlDefineTypes.SQLite);
+        }
+    }
+}

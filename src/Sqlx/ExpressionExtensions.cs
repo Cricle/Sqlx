1c7af55aea7ca3b9206ea079c3ecd7c3c639c201
HCricle <15218450198@163.com>
2026-01-12 22:46:44 +0800

    refactor: optimize ExpressionExtensions - Replace reflection with Expression.Lambda().Compile() - Apply Microsoft code formatting standards - Reduce code from 137 to 107 lines - Use pattern matching for cleaner switch expressions

--- src/Sqlx/ExpressionExtensions.cs
+++ src/Sqlx/ExpressionExtensions.cs
@@ -1,3 +1,9 @@
+// -----------------------------------------------------------------------
+// <copyright file="ExpressionExtensions.cs" company="Cricle">
+// Copyright (c) Cricle. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
@@ -5,17 +11,17 @@ using System.Linq.Expressions;
 namespace Sqlx
 {
     /// <summary>
-    /// Extension methods for Expression to SQL conversion
+    /// Extension methods for Expression to SQL conversion.
     /// </summary>
     public static class ExpressionExtensions
     {
         /// <summary>
-        /// Converts a predicate expression to a SQL WHERE clause
+        /// Converts a predicate expression to a SQL WHERE clause.
         /// </summary>
-        /// <typeparam name="T">Entity type</typeparam>
-        /// <param name="predicate">The predicate expression</param>
-        /// <param name="dialect">SQL dialect (default: SQLite)</param>
-        /// <returns>SQL WHERE clause without "WHERE" keyword</returns>
+        /// <typeparam name="T">Entity type.</typeparam>
+        /// <param name="predicate">The predicate expression.</param>
+        /// <param name="dialect">SQL dialect (default: SQLite).</param>
+        /// <returns>SQL WHERE clause without "WHERE" keyword.</returns>
         public static string ToWhereClause<T>(
             this Expression<Func<T, bool>> predicate,
             SqlDialect? dialect = null)
@@ -25,124 +31,86 @@ namespace Sqlx
                 return string.Empty;
             }
 
-            var actualDialect = dialect ?? SqlDefine.SQLite;
-            var converter = ExpressionToSql<T>.Create(actualDialect);
-            converter.Where(predicate);
-            
-            // Use ToWhereClause() to get just the WHERE condition
-            return converter.ToWhereClause();
+            return ExpressionToSql<T>.Create(dialect ?? SqlDefine.SQLite)
+                .Where(predicate)
+                .ToWhereClause();
         }
 
         /// <summary>
-        /// Extracts parameters from a predicate expression
+        /// Extracts parameters from a predicate expression.
         /// </summary>
-        /// <typeparam name="T">Entity type</typeparam>
-        /// <param name="predicate">The predicate expression</param>
-        /// <returns>Dictionary of parameter names and values</returns>
+        /// <typeparam name="T">Entity type.</typeparam>
+        /// <param name="predicate">The predicate expression.</param>
+        /// <returns>Dictionary of parameter names and values.</returns>
         public static Dictionary<string, object?> GetParameters<T>(
             this Expression<Func<T, bool>> predicate)
         {
             var parameters = new Dictionary<string, object?>();
-            
-            if (predicate == null)
+
+            if (predicate != null)
             {
-                return parameters;
+                ExtractParameters(predicate.Body, parameters);
             }
 
-            // Extract parameters from the expression
-            ExtractParameters(predicate.Body, parameters);
-            
             return parameters;
         }
 
-        private static void ExtractParameters(Expression expression, Dictionary<string, object?> parameters)
+        private static void ExtractParameters(Expression? expression, Dictionary<string, object?> parameters)
         {
             if (expression == null)
             {
                 return;
             }
 
-            switch (expression.NodeType)
+            switch (expression)
             {
-                case ExpressionType.Equal:
-                case ExpressionType.NotEqual:
-                case ExpressionType.GreaterThan:
-                case ExpressionType.GreaterThanOrEqual:
-                case ExpressionType.LessThan:
-                case ExpressionType.LessThanOrEqual:
-                    var binary = (BinaryExpression)expression;
+                case BinaryExpression binary:
                     ExtractParameters(binary.Left, parameters);
                     ExtractParameters(binary.Right, parameters);
                     break;
 
-                case ExpressionType.AndAlso:
-                case ExpressionType.OrElse:
-                    var logicalBinary = (BinaryExpression)expression;
-                    ExtractParameters(logicalBinary.Left, parameters);
-                    ExtractParameters(logicalBinary.Right, parameters);
+                case ConstantExpression { Value: not null } constant:
+                    parameters[$"p{parameters.Count}"] = constant.Value;
                     break;
 
-                case ExpressionType.Constant:
-                    var constant = (ConstantExpression)expression;
-                    if (constant.Value != null)
+                case MemberExpression { Expression: ConstantExpression } member:
+                    var value = EvaluateExpression(member);
+                    if (value != null)
                     {
-                        var paramName = $"p{parameters.Count}";
-                        parameters[paramName] = constant.Value;
+                        parameters[$"p{parameters.Count}"] = value;
                     }
+
                     break;
 
-                case ExpressionType.MemberAccess:
-                    var member = (MemberExpression)expression;
-                    if (member.Expression != null && member.Expression.NodeType == ExpressionType.Constant)
-                    {
-                        var constExpr = (ConstantExpression)member.Expression;
-                        var value = GetMemberValue(member, constExpr.Value);
-                        if (value != null)
-                        {
-                            var paramName = $"p{parameters.Count}";
-                            parameters[paramName] = value;
-                        }
-                    }
-                    break;
-
-                case ExpressionType.Call:
-                    var methodCall = (MethodCallExpression)expression;
+                case MethodCallExpression methodCall:
                     if (methodCall.Object != null)
                     {
                         ExtractParameters(methodCall.Object, parameters);
                     }
+
                     foreach (var arg in methodCall.Arguments)
                     {
                         ExtractParameters(arg, parameters);
                     }
+
                     break;
 
-                case ExpressionType.Not:
-                    var unary = (UnaryExpression)expression;
+                case UnaryExpression unary:
                     ExtractParameters(unary.Operand, parameters);
                     break;
             }
         }
 
-        private static object? GetMemberValue(MemberExpression member, object? container)
+        private static object? EvaluateExpression(Expression expression)
         {
-            if (container == null)
+            try
+            {
+                return Expression.Lambda(expression).Compile().DynamicInvoke();
+            }
+            catch
             {
                 return null;
             }
-
-            if (member.Member is System.Reflection.FieldInfo field)
-            {
-                return field.GetValue(container);
-            }
-
-            if (member.Member is System.Reflection.PropertyInfo property)
-            {
-                return property.GetValue(container);
-            }
-
-            return null;
         }
     }
 }
-
